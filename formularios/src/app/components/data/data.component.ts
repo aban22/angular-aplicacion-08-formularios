ca2add76735543a0805ba8d507aab7d300021c26
Abel Fernandez <abelfergodevelop@gmail.com>
2019-02-21 20:43:34 +0100

    Clase 172

--- formularios/src/app/components/data/data.component.ts
+++ formularios/src/app/components/data/data.component.ts
@@ -12,14 +12,20 @@ export class DataComponent {
 
     constructor() {
         this.formulario = new FormGroup({
-            nombre: new FormControl('Fernando'),
-            apellido: new FormControl(),
-            correo: new FormControl()
+            nombre:     new FormControl(null,   Validators.required),
+            apellido:   new FormControl(null,   Validators.required),
+            correo:     new FormControl(
+                                        null,
+                                        [
+                                            Validators.required,
+                                            Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+                                        ])
         });
     }
 
     guardarCambios() {
         console.log(this.formulario.value);
+        console.log(this.formulario);
     }
 
 }
